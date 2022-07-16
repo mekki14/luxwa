c378c5b144a2cdf6ad81c19ffb7f130eaf3ab1d2
unknown <fmekki2002@gmail.com>
2022-07-16 14:36:44 +0100

    Created The main project structure

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const openNav =document.querySelector('.open-btn')
+const closeNav =document.querySelector('.close-btn')
+const menu =document.querySelector('.nav-list')
+openNav.addEventListener("click",()=>{
+    menu.classList.add("show")
+})
+
+closeNav.addEventListener("click",()=>{
+    menu.classList.remove("show")
+})
+
+
+//fix Nav
+const navBar= document.querySelector(".nav");
+const navHeight =navBar.getBoundingClientRect().height;
+window.addEventListener("scroll",()=>
+{
+    const scrollHeight =window.pageYOffset;
+    if(scrollHeight>navHeight){
+        navBar.classList.add("fix-nav");
+    }
+    else {
+        navBar.classList.remove("fix-nav");
+    }
+})
---
+++ js/slider.js
@@ -0,0 +1,54 @@
+const swiper1 = new Swiper(".slider-1",{
+  autoplay:{
+    delay:3500,
+    disableOnInteraction:false,
+  },
+  grapbCursor: true,
+  effect: 'fade',
+  loop: true,
+  navigation :{
+    nextEl:".swiper-next",
+    prevEl:".swiper-prev" 
+  }
+});
+
+const swiper2 = new Swiper(".slider-2",{
+  grapbCursor: true,
+  spaceBetween:30,
+  navigation :{
+    nextEl:".custom-next",
+    prevEl:".custom-prev" 
+  },
+  breakpoints:{
+    640:{
+      slidesPerView:2,
+    },
+    768:{
+      slidesPerView:3,
+    },
+    1024:{
+      slidesPerView:2,
+    }
+  }
+});
+
+
+const swiper3 = new Swiper(".slider-3",{
+  loop:true,
+  grapbCursor: true,
+  autoplay:{
+    delay:3500,
+    disableOnInteractionfalse,
+  },
+  spaceBetween:30,
+  slidesPerView:2,
+  breakpoints:{
+    
+    768:{
+      slidesPerView:3,
+    },
+    1024:{
+      slidesPerView:2,
+    }
+  }
+});
